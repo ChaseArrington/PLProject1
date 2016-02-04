1178864b63cc9957cfa9b80ad24b57b19a92fbec
Adam Barrett <ke5gpd@gmail.com>
2016-02-03 20:01:55 -0600

    no message

--- src/arrington/customer/CustomerApp.java
+++ src/arrington/customer/CustomerApp.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 
 public class CustomerApp {
 
-    public static void
-main(String args[]){
-  // Printing the initial statements
+    public static void main(String args[]){
         System.out.println("Welcome to the Customer Viewer");
-        System.out.println("");
+        Scanner user_input = new Scanner( System.in );
+        String Continue;
+        Continue = "y";
+        while(Continue == "y") {
+            String CustomerID;
+            System.out.println("");
+            System.out.print("Enter a Customer Number: ");
+            CustomerID = user_input.next();
+            int IDNum = Integer.parseInt(CustomerID);
+            System.out.println("");
+            System.out.println("Customer Info Here");
+            System.out.println("");
+            System.out.print("Display another customer? (y/n): ");
+            Continue = user_input.next();
+        }
+
     }
 
 }
